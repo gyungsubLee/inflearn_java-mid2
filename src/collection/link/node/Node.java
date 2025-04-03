a593a822782232e9586a4a3665aedd45ede17dd0
gyoungsubLee <dltjq2323@gmail.com>
2025-04-04 01:21:25 +0900

    4.4 - 연결리스트 기본 구현

--- src/collection/link/node/Node.java
+++ src/collection/link/node/Node.java
@@ -2,8 +2,8 @@ package collection.link.node;
 
 public class Node {
 
-    Object item;
-    Node next;
+    public Object item;
+    public Node next;
 
     public Node(Object item) {
         this.item = item;
